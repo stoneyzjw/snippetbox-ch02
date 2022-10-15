84511d8d29e81f0c342d1e7b538dc6739d902983
zhengjingwei <zhengjingwei@aliyun.com>
2022-10-15 10:14:06 +0800

    add customized header check

--- main.go
+++ main.go
@@ -23,6 +23,17 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+    // Use r.Method to check whether the request is using POST or not. 
+    if r.Method != "POST" {
+        /* If it's not, use the w.WriteHeader() method to send a 405 status 
+         * code and the w.Write() method to write a "Method Not Allowed" 
+         * response body. We then return from the function so that the 
+         * subsequent code is not executed 
+         */ 
+        w.WriteHeader(405)
+        w.Write([]byte("Method Not Allowed\n"))
+        return
+    }
     w.Write([]byte("Write a new snippet\n"))
 }
 
